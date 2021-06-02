6d8acd714d746d6591f5387d565890a6b7af72be
Islam Nuryyev <islamn2578@gmail.com>
2021-06-03 00:51:05 +0500

    removed redux-logger from deployment build

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,10 +4,15 @@ import { persistStore } from "redux-persist"; //allows a browser to cash the sto
 //We need to add Middleware to our store so that whenever an action gets fired or dispatched we can catch and display them, it is helpful for debugging our redux, it catches the actions and console.logs them
 //Middleware are functions that take actions in, do something with them and pass them into the Root Reducer
 //in order to use Middleware, we have to import logger
-import logger from "redux-logger";
+import logger from "redux-logger"; 
 import rootReducer from "./root_reducer";
 
-const middlewares = [logger];
+const middlewares = [];
+
+//this condition will make sure that actions are shown in console only in the development stage of our application, and not in the deployment stage
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger)
+}
 
 //creating the store
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
