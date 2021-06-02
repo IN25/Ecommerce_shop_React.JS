f620b3c3827800c426ab8ba4aca0a22fb998bf04
Islam Nuryyev <islamn2578@gmail.com>
2021-06-02 21:52:39 +0500

    modified mapping in the CollectionsOverview component

--- src/components/collections_overview/collections_overview.component.js
+++ src/components/collections_overview/collections_overview.component.js
@@ -5,11 +5,14 @@ import { connect } from "react-redux";
 import { PreviewCollection } from "../preview_collection/collection_preview.component";
 
 const CollectionsOverview = ({ collections }) => {
-  console.log("collections = ", collections);
+  //because the collections state is object, we cannot map thorough it, so we have to iterate thorough keys and put them into an array
+  const collectionArray = Object.keys(collections).map(
+    (key) => collections[key]
+  );
+
   return (
     <div className="collections_overview">
-      {collections.map((collection) => {
-        console.log("collection = ", collection);
+      {collectionArray.map((collection) => {
         return (
           <PreviewCollection
             key={collection.id}
