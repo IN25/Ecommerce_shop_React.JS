4bcd463c6d992af61b777a4079dea692799bc7ea
Islam Nuryyev <islamn2578@gmail.com>
2021-06-12 22:33:25 +0500

    commit

--- src/components/custom_button/custom_button.component.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import { CustomButtonContainer } from "./custom_button.styles";
-
-export const CustomButton = ({ value, ...otherProps }) => (
-  <CustomButtonContainer {...otherProps}>{value}</CustomButtonContainer>
-);
